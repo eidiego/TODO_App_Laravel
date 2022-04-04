70b0010488c4f4a7c7bfe81bd9cbf80fe08bbc81
Diego Rodrigues <diegokod@gmail.com>
2022-04-04 20:59:18 -0300

    Mensagem teste configurada

--- resources/views/emails/mensagem-teste.blade.php
+++ resources/views/emails/mensagem-teste.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
-# Introduction
+# Introdução
 
-The body of your message.
+Corpo da mensagem.
 
 @component('mail::button', ['url' => ''])
-Button Text
+Texto do botão
 @endcomponent
 
-Thanks,<br>
+Obrigado,<br>
 {{ config('app.name') }}
 @endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTesteMail;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+
+Route::get('/mensagem-teste', function(){
+    return new MensagemTesteMail();
+});
