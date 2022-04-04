bbfde190339c4f1b46e1ddd19a6d37c388cf8033
Diego Rodrigues <diegokod@gmail.com>
2022-04-03 23:47:10 -0300

    Auth methods add

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
