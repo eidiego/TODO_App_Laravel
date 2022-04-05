d7b049b8dddd6e0bff617cc9ac89bdb182c7eeb5
Diego Rodrigues <diegokod@gmail.com>
2022-04-04 21:19:53 -0300

    notification add

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token) {
+        dd('chegamos aqui');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
 
 Route::get('/mensagem-teste', function(){
-    // return new MensagemTesteMail();
-    Mail::to('diegokod@gmail.com')->send(new MensagemTesteMail());
-    return 'E-mail enviado com sucesso!';
+    return new MensagemTesteMail();
+    // Mail::to('diegokod@gmail.com')->send(new MensagemTesteMail());
+    // return 'E-mail enviado com sucesso!';
 });
