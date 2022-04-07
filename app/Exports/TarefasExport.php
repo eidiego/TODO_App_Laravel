61c46d0d442911a4584f81229434bcf3849290a7
Diego Rodrigues <diegokod@gmail.com>
2022-04-06 23:00:36 -0300

    Include Headings exports

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,16 @@ class TarefasExport implements FromCollection
     {
         // return Tarefa::all();
         return auth()->user()->tarefas()->get();
+    }
+
+    public function Headings():array {
+        return [
+           'ID da tarefas',
+           'ID do usuário',
+           'Tarefa',
+           'Data limite conclusão',
+           'Data criação',
+           'Data atualização',
+        ];
     }
 }
